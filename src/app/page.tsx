7b408497007451c4ea9b7bb1e8fe44c1ed0ded9c
young <rhsiddlsidhd@nate.com>
2025-05-12 12:15:42 +0900

    styled components reponsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { styled } from "styled-components";
 export default function Home() {
   return (
     <>
-      <Container>123</Container>
+      <Container>1213</Container>
     </>
   );
 }
--- src/app/utils/styles.ts
+++ src/app/utils/styles.ts
@@ -1,26 +1,40 @@
-import theme from "../themes";
-import { Responsive, ResponsiveProp } from "../types";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
 
-type AppTheme = typeof theme;
+import theme from "../themes";
+import { Responsive, ResponsiveProp } from "../types/styles";
+
+// Theme의 타입
+export type AppTheme = typeof theme;
+
 type SpaceThemeKeys = keyof typeof theme.space;
 type ColorThemeKeys = keyof typeof theme.colors;
 type FontSizeThemeKeys = keyof typeof theme.fontSizes;
 type LetterSpacingThemeKeys = keyof typeof theme.letterSpacings;
 type LineHeightThemeKeys = keyof typeof theme.lineHeights;
 
+// 각 Theme의 키의 타입
 export type Space = SpaceThemeKeys | (string & {});
 export type Color = ColorThemeKeys | (string & {});
 export type FontSize = FontSizeThemeKeys | (string & {});
 export type LetterSpacing = LetterSpacingThemeKeys | (string & {});
 export type LineHeight = LineHeightThemeKeys | (string & {});
 
+// 브레이크 포인트
 const BREAKPOINTS: { [key: string]: string } = {
-  sm: "640px",
-  md: "768px",
-  lg: "1024px",
-  xl: "1280px",
+  sm: "640px", // 640px 이상
+  md: "768px", // 768px 이상
+  lg: "1024px", // 1024px 이상
+  xl: "1280px", // 1280px 이상
 };
 
+/**
+ * Responsive 타입을 CSS 속성과 그 값으로 변환
+ * @param propKey CSS 속성
+ * @param prop Responsive 타입
+ * @param theme AppTheme
+ * @returns CSS 속성과 그 값(ex. background-color: white;)
+ */
 export function toPropValue<T>(
   propKey: string,
   prop?: Responsive<T>,
@@ -69,6 +83,7 @@ const SPACE_KEYS = new Set([
   "margin-bottom",
   "margin-right",
   "padding",
+  "padding-top",
   "padding-left",
   "padding-bottom",
   "padding-right",
@@ -78,6 +93,13 @@ const FONT_SIZE_KEYS = new Set(["font-size"]);
 const LINE_SPACING_KEYS = new Set(["letter-spacing"]);
 const LINE_HEIGHT_KEYS = new Set(["line-height"]);
 
+/**
+ * Theme에 지정된 CSS 속성값으로 변환
+ * @param propKey CSS 속성
+ * @param value CSS 속성값
+ * @param theme AppTheme
+ * @returns CSS 속성값
+ */
 function toThemeValueIfNeeded<T>(propKey: string, value: T, theme?: AppTheme) {
   if (
     theme &&
